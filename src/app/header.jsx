3641b314ad859bbeaa76f9c72b59f25e20c47dba
Philip <95943570+Srcyax@users.noreply.github.com>
2024-02-08 23:31:42 -0300

    a

--- src/app/header.jsx
+++ src/app/header.jsx
@@ -1,3 +1,4 @@
+import Link from 'next/link'
 
 export default function Header(){
     return (
@@ -6,8 +7,8 @@ export default function Header(){
               <div className="flex flex-row justify-between items-center px-5 py-4">
                   <h1 className="text-3xl transition 1.3s text-white hover:text-purple">&lt;Guedes/&gt;</h1>
                 <div className="flex flex-row gap-5">
-                  <a href="" className="transition 1.3s text-white hover:text-purple">about-me</a>
-                  <a href="" className="transition 1.3s text-white hover:text-purple">projects</a>
+                  <Link href="/about" className="transition 1.3s text-white hover:text-purple">about-me</Link>
+                  <Link href="/about" className="transition 1.3s text-white hover:text-purple">projects</Link>
                 </div>
               </div> 
           </header>  
