0e5a164268328fc2ed35ff7c200da8e9e1683443
Philip <95943570+Srcyax@users.noreply.github.com>
2024-02-09 01:58:04 -0300

    a

--- src/app/(pages)/projects/card.tsx
+++ src/app/(pages)/projects/card.tsx
@@ -1,9 +1,37 @@
-export default function CardProject({image, title} : {image : string, title : string}){
+export default function CardProject({image, title, desc, technologies} : {image : string, title : string, desc : string, technologies : string[]}){
+    
+    const technologiesUrl = [
+        ["java", "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/java/java-plain.svg"],
+        ["c#", "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/csharp/csharp-original.svg"],
+        ["c++", "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/cplusplus/cplusplus-original.svg"],
+        ["javascript", "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/javascript/javascript-original.svg"],
+        ["react", "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/react/react-original.svg"],
+        ["typescript", "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/typescript/typescript-original.svg"],
+        ["html5", "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/html5/html5-original.svg"],
+        ["mongodb", "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/mongodb/mongodb-original.svg"],
+        ["css3", "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/css3/css3-original.svg"],
+        ["nextjs", "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/nextjs/nextjs-original.svg"],
+        ["tailwindcss", "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/tailwindcss/tailwindcss-original.svg"]
+    ]
+
     return (
-        <div className="border-2 border-indigo-600 rounded-lg border-purple opacity-50 hover:opacity-100 transition 1.5s w-60 h-auto p-5 flex flex-col items-center">
+        <div className="m-12 border-2 border-indigo-600 rounded-lg border-purple opacity-50 hover:opacity-100 transition 1.5s w-60 h-auto p-5 flex flex-col items-center">
             <img className="p-5" width={200} src={image} alt="" />
-            <h1 className="text-2xl relative right-20">{title}</h1>
-            <p className="text-center text-wrap text-gray">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsum adipisci ut aliquam mollitia totam nam fugit illo esse minima ex vel, maiores quo repellat, et voluptas perspiciatis repellendus.</p>
+            <div className="flex flex-col items-center justify-between gap-1">
+                <h1 className="text-2xl">{title}</h1>
+                <div className="flex flex-row gap-1">
+                {
+                    technologies.map((tech, index) => (
+                    <div key={index}>
+                        {
+                        technologiesUrl.filter(t => t[0] === tech).map((filteredTech, index) => (
+                                <img key={index} className="opacity-70 hover:opacity-100 hover:w-6 transition-all duration-200 ease" width={20} src={filteredTech[1]} title={filteredTech[0]} />
+                        ))}
+                    </div>
+                ))}
+                </div>
+            </div>
+            <p className="text-center text-wrap text-gray py-5">{desc}</p>
         </div>
     )
 }
--- src/app/(pages)/projects/page.tsx
+++ src/app/(pages)/projects/page.tsx
@@ -1,11 +1,38 @@
 import CardProject from "./card"
 
 export default function Projects(){
+
     return (
         <main>
             <div className="flex flex-col items-center">
                 <h1 className="text-3xl mt-10">Projects</h1>
-                <CardProject image="https://placehold.jp/150x150.png" title = "Test"/>
+                <div className="grid grid-cols-3 grid-rows-3">
+                    <CardProject 
+                        image="https://cdn.discordapp.com/attachments/855625092302503946/860960024788074536/unknown.png?ex=65d10401&is=65be8f01&hm=e0aca38d4414e0079f4dead30d5f3dcafa6b443a4ac7bdb8659f358544cb3cc4&" 
+                        title = "Calculator"
+                        desc="A calculator project" 
+                        technologies={
+                            ["html5", "css3", "javascript"]
+                        }
+                    />
+                    <CardProject 
+                        image="https://cdn.discordapp.com/attachments/855625092302503946/860960024788074536/unknown.png?ex=65d10401&is=65be8f01&hm=e0aca38d4414e0079f4dead30d5f3dcafa6b443a4ac7bdb8659f358544cb3cc4&" 
+                        title = "Forum Yamacha"
+                        desc="A forum project for my future cheat" 
+                        technologies={
+                            ["mongodb", "tailwindcss", "nextjs", "react"]
+                        }
+                    />
+                    <CardProject 
+                        image="https://cdn.discordapp.com/attachments/855625092302503946/860960024788074536/unknown.png?ex=65d10401&is=65be8f01&hm=e0aca38d4414e0079f4dead30d5f3dcafa6b443a4ac7bdb8659f358544cb3cc4&" 
+                        title = "Test"
+                        desc="Olá mundo" 
+                        technologies={
+                            ["react", "c++", "c#"]
+                        }
+                    />
+                </div>
+                
             </div>
         </main>                             
     )
