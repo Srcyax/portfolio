719fbb1c22ee8a5121dcb323785502c28fd1541e
Philip <95943570+Srcyax@users.noreply.github.com>
2024-02-09 13:40:48 -0300

    Update card.tsx

--- src/app/(pages)/projects/card.tsx
+++ src/app/(pages)/projects/card.tsx
@@ -16,7 +16,7 @@ export default function CardProject({image, title, desc, technologies} : {image 
     ]
 
     return (
-        <div className="m-12 border-2 border-indigo-600 rounded-lg border-purple opacity-50 hover:opacity-100 w-60 h-auto p-5 flex flex-col items-center transition-all duration-200 ease">
+        <div className="m-12 border-2 border-indigo-600 rounded-lg border-purple opacity-50 hover:opacity-100 w-60 h-auto p-5 flex flex-col items-center transition-all duration-200 ease"> 
             <img className="p-5 w-40 h-40 rounded-full" src={image} alt="" />
             <div className="flex flex-col items-center justify-between gap-1">
                 <h1 className="text-2xl">{title}</h1>
@@ -26,13 +26,21 @@ export default function CardProject({image, title, desc, technologies} : {image 
                     <div key={index}>
                         {
                         technologiesUrl.filter(t => t[0] === tech).map((filteredTech, index) => (
-                            <img key={index} className="opacity-70 hover:opacity-100 hover:w-6 transition-all duration-200 ease" width={20} src={filteredTech[1]} title={filteredTech[0]} />
+                            <img key={index} className="opacity-70 hover:opacity-100 hover:w-5 transition-all duration-200 ease" width={22} src={filteredTech[1]} title={filteredTech[0]} />
                         ))}
                     </div>
                 ))}
                 </div>
             </div>
             <p className="text-center text-wrap text-gray py-5">{desc}</p>
+            <div className="flex flex-row mt-auto gap-3">
+                <button className="text-[12px] hover:text-[13px] border-2 border-indigo-600 border-purple bg-transparent hover:bg-purple hover:border-transparent w-16 h-8 rounded-lg transition-all duration-500 ease">
+                    Site
+                </button>
+                <button className="text-[12px] hover:text-[13px] border-2 border-indigo-600 border-purple bg-transparent hover:bg-purple hover:border-transparent w-16 h-8 rounded-lg transition-all duration-500 ease">
+                    Github
+                </button>
+            </div>
         </div>
     )
 }
