89e79a1d08f16b85a0616f9cbefc0ee32b8793e1
Philip <95943570+Srcyax@users.noreply.github.com>
2024-02-09 02:23:31 -0300

    Update header.jsx

--- src/app/header.jsx
+++ src/app/header.jsx
@@ -5,7 +5,7 @@ export default function Header(){
       <div>
           <header>
               <div className="flex flex-row justify-between items-center px-5 py-4">
-                  <h1 className="text-3xl transition 1.3s text-white hover:text-purple">&lt;Guedes/&gt;</h1>
+              <Link href="/" className="text-3xl transition 1.3s text-white hover:text-purple">&lt;Guedes/&gt;</Link>
                 <div className="flex flex-row gap-5">
                   <Link href="/about" className="transition 1.3s text-white hover:text-purple">about-me</Link>
                   <Link href="/projects" className="transition 1.3s text-white hover:text-purple">projects</Link>
