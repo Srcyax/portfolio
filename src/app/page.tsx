fb4e5f767760be4df85989d57762a101e7af648b
Philip <95943570+Srcyax@users.noreply.github.com>
2024-02-10 15:57:43 -0300

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,9 +5,9 @@ export default function Home() {
 
   return (
     <main className="flex flex-col items-center justify-between p-14 min-h-52">
-        <div className="flex flex-row justify-between items-center gap-60">
+        <div className="flex flex-col xl:flex-row justify-between items-center gap-0 xl:gap-60">
             <div className="flex flex-col justify-between items-center min-w-86 max-w-96 mb-40">
-              <h1 className="text-7xl py-3">Hi there 👋</h1>
+              <h1 className="text-5xl md:text-7xl xl:text-6xl py-3">Hi there 👋</h1>
               <h1 className="text-gray text-center">Welcome to my portfolio, here you will find some projects developed by me</h1>
               <div className="flex flex-row gap-5 mt-5">
                   <a href="http://" target="_blank" rel="noopener noreferrer">
