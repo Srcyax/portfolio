87a2d643849e170913c59e834f9bddb38599b0f5
Philip <95943570+Srcyax@users.noreply.github.com>
2024-02-14 15:39:43 -0300

    Update page.tsx

--- src/app/(pages)/projects/page.tsx
+++ src/app/(pages)/projects/page.tsx
@@ -12,7 +12,15 @@ export default function Projects(){
                         title = "Portfolio"
                         desc="My portfolio project" 
                         technologies={
-                            ["nextjs", "tailwindcss", "nodejs", "javascript", "react"]
+                            ["nextjs", "tailwindcss", "javascript", "react"]
+                        }
+                    />
+                    <CardProject 
+                        image="https://w7.pngwing.com/pngs/399/491/png-transparent-quiz-computer-icons-general-knowledge-question-others-blue-game-angle.png" 
+                        title = "Quizz Hub"
+                        desc="Test your knowledge on QuizzHub" 
+                        technologies={
+                            ["nextjs", "tailwindcss", "javascript", "react"]
                         }
                     />
                     <CardProject 
@@ -37,14 +45,6 @@ export default function Projects(){
                         desc="A small old LAN multi-player FPS horror game project" 
                         technologies={
                             ["unity", "c#"]
-                        }
-                    />
-                    <CardProject 
-                        image="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAOEAAADhCAMAAAAJbSJIAAAAMFBMVEXp7vG6vsG3u77s8fTCxsnn7O/f5OfFyczP09bM0dO8wMPk6ezY3eDd4uXR1tnJzdBvAX/cAAACVElEQVR4nO3b23KDIBRA0ShGU0n0//+2KmO94gWZ8Zxmr7fmwWEHJsJUHw8AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAwO1MHHdn+L3rIoK6eshsNJ8kTaJI07fERPOO1Nc1vgQm2oiBTWJ+d8+CqV1heplLzMRNonED+4mg7L6p591FC+133/xCRNCtd3nL9BlxWP++MOaXFdEXFjZ7r8D9l45C8y6aG0cWtP/SUGhs2d8dA/ZfGgrzYX+TVqcTNRRO9l+fS5eSYzQs85psUcuzk6igcLoHPz2J8gvzWaH/JLS+95RfOD8o1p5CU5R7l5LkfKEp0mQ1UX7hsVXqDpRrifILD/3S9CfmlUQFhQfuFu0STTyJ8gsP3PH7GVxN1FC4t2sbBy4TNRTu7LyHJbqaqKFw+/Q0ncFloo7CjRPwMnCWqKXQZ75El4nKC9dmcJaou9AXOE5UXbi+RGeJygrz8Uf+GewSn9uXuplnWDZJ7d8f24F/s6iq0LYf9olbS3Q8i5oKrRu4S9ybwaQ/aCkqtP3I28QDgeoK7TBya/aXqL5COx67PTCD2grtdOwH+pQV2r0a7YVBgZoKwwIVFQYG6ikMDVRTGByopjD8ATcKb0UhhRTe77sKs2DV7FKSjId18TUEBYVyLhUThWfILHTDqmI85/2RWWjcE/bhP6OD7maT3h20MHsA47JC3PsW0wcwLhv9t0OOPOIkCn21y2bXXwlyylxiYMPk1SuCSmpfK8bNQvIrpAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAADwNX4BCbAju9/X67UAAAAASUVORK5CYII=" 
-                        title = "Coming soon"
-                        desc="empty..." 
-                        technologies={
-                            ["none"]
                         }
                     />
                     <CardProject 
