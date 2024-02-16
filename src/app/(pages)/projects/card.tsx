09c1d136d9cd8833b3206799c706036cc6008a1e
Philip <95943570+Srcyax@users.noreply.github.com>
2024-02-15 23:07:04 -0300

    Lazy image

--- src/app/(pages)/projects/card.tsx
+++ src/app/(pages)/projects/card.tsx
@@ -1,7 +1,24 @@
+"use client"
+
 import { Github, Globe, Key } from 'lucide-react';
+import { useState } from 'react';
+import { Skeleton } from "@/components/ui/skeleton"
+import { LazyLoadImage } from 'react-lazy-load-image-component';
 
 export default function CardProject({image, title, desc, technologies, github, site} : {image : string, title : string, desc : string, technologies : string[], github : string, site : string}){
     
+    const [imageLoaded, setImageLoaded] = useState({
+        projectImage: false,
+        techImage: false
+      });
+    
+      const handleImageLoad = (imageType: string) => {
+        setImageLoaded(prevState => ({
+          ...prevState,
+          [imageType]: true
+        }));
+      };
+
     const technologiesUrl = [
         ["c#", "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/csharp/csharp-original.svg"],
         ["unity", "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/unity/unity-original.svg"],
@@ -18,7 +35,8 @@ export default function CardProject({image, title, desc, technologies, github, s
 
     return (
         <div className="m-12 border-2 border-indigo-600 rounded-lg border-purple hover:shadow-neon opacity-50 hover:opacity-100 w-60 h-auto p-5 flex flex-col items-center transition-all duration-200 ease"> 
-            <img loading="lazy" className="p-5 w-40 h-40 rounded-full" src={image} alt="project image" />
+            {!imageLoaded.projectImage ? <Skeleton className='w-40 h-40 rounded-full'/> : null}
+            <LazyLoadImage className={imageLoaded.projectImage ? 'w-40 h-40 rounded-full' : ''} src={image} beforeLoad={() => handleImageLoad('projectImage')}/>
             <div className="flex flex-col items-center justify-between gap-1">
                 <h1 className="text-2xl">{title}</h1>
                 <div className="flex flex-row gap-1 w-40 h-6 justify-center">
@@ -27,12 +45,11 @@ export default function CardProject({image, title, desc, technologies, github, s
                     <div key={index}>
                         {
                         technologiesUrl.filter(t => t[0] === tech).map((filteredTech, index) => (
-                            <>
-                                <div key={index} className="group grid place-items-center">
-                                    <img key={index} loading="lazy" className="opacity-70 hover:opacity-100 hover:w-5 transition-all duration-200 ease" width={22} src={filteredTech[1]} />
-                                    <p key={index} className="text-[1px] group-hover:text-[10px] opacity-0 group-hover:opacity-100 transition-all duration-200 ease">{filteredTech[0]}</p>
-                                </div>
-                            </>
+                            <div key={index} className="group grid place-items-center">
+                                {!imageLoaded.techImage ? <Skeleton className='w-6 h-6 rounded-full'/> : null}
+                                <LazyLoadImage className={imageLoaded.techImage ? 'opacity-70 hover:opacity-100 hover:w-5 transition-all duration-200 ease' : ''} width={22} src={filteredTech[1]} beforeLoad={() => handleImageLoad('techImage')}/>
+                                <p key={index} className="text-[1px] group-hover:text-[10px] opacity-0 group-hover:opacity-100 transition-all duration-200 ease">{filteredTech[0]}</p>
+                            </div>
                         ))}
                     </div>
                 ))}
