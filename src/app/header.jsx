b31982a26442cde39aef03bc620196a2d1548c20
Philip <95943570+Srcyax@users.noreply.github.com>
2024-02-15 23:37:49 -0300

    fixed

--- src/app/header.jsx
+++ src/app/header.jsx
@@ -2,21 +2,17 @@ import Link from "next/link"
 
 export default function Header(){
     return (
-      <div>
-          <header>
-              <div className="flex flex-row justify-between items-center m-5">
-                <Link href="/" className="group text-2xl transition 1.3s text-white w-60">
-                  <strong className="group-hover:p-1 group-hover:text-purple transition-all duration-200">&lt;</strong>
-                  Philipe
-                  <strong className="group-hover:p-1 group-hover:text-purple transition-all duration-200">/&gt;</strong>
-                </Link>
-                <div className="flex flex-row gap-5">
-                  <Link href="/" className="transition 1.3s text-white hover:text-purple">Home</Link>
-                  <Link href="/about" className="transition 1.3s text-white hover:text-purple">About</Link>
-                  <Link href="/projects" className="transition 1.3s text-white hover:text-purple">Projects</Link>
-                </div>
-              </div> 
-          </header>  
-      </div>
+        <header className="flex flex-row justify-between items-center p-5">
+          <Link href="/" className="group text-2xl transition 1.3s text-white w-60">
+            <strong className="group-hover:p-1 group-hover:text-purple transition-all duration-200">&lt;</strong>
+            Philipe
+            <strong className="group-hover:p-1 group-hover:text-purple transition-all duration-200">/&gt;</strong>
+          </Link>
+          <div className="flex flex-row gap-5">
+            <Link href="/" className="transition 1.3s text-white hover:text-purple">Home</Link>
+            <Link href="/about" className="transition 1.3s text-white hover:text-purple">About</Link>
+            <Link href="/projects" className="transition 1.3s text-white hover:text-purple">Projects</Link>
+          </div>
+      </header>  
     )
   }
